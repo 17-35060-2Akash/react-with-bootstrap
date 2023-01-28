0572c6dc39843acc1c40ec733ea5ad3e99c1e2f1
Akash <na.akash715@gmail.com>
2023-01-29 02:59:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Spinner } from 'react-bootstrap';
+import GridCard from './components/GridCard/GridCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+      <Button variant='danger'>Heelo</Button>
+      <br /><br /><br /><br />
+      <br /><br />      <Spinner animation="border" size="sm" />
+      <Spinner animation="border" />
+      <Spinner animation="grow" size="sm" />
+      <Spinner animation="grow" />
+      <button type="button" className="btn btn-link">Link</button>
+      <br /><br /><br /><br />
+      <GridCard></GridCard>
     </div>
   );
 }
